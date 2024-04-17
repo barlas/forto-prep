830ba8a9b4046f7bf5e5b9c1304108b8dd7d94e0
aries <barlasss@gmail.com>
2024-04-16 23:59:04 -0700

    chore: dist deleted.

--- dist/models/User.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-// Create a Schema corresponding to the document interface.
-const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    age: { type: Number, required: true }
-});
-// Create a Model.
-const User = mongoose.model('User', userSchema);
-export default User;
